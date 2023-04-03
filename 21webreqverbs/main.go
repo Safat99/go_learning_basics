e1a375687f4ec93285a29b82de98fc46d738f65f
Safat99 <sarkersafat99@gmail.com>
2023-04-03 13:17:02 +0600

    postform request sent and learnt

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -11,7 +12,8 @@ func main() {
 
 	fmt.Println("Welcome to web verb video - LCO")
 	//PerformGetRequest()
-	PerformPostJsonRequest()
+	// PerformPostJsonRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetRequest() {
@@ -64,4 +66,28 @@ func PerformPostJsonRequest() {
 	content, _ := ioutil.ReadAll(response.Body)
 
 	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+
+	const myurl = "http://localhost:8000/postform"
+
+	// creating formdata
+
+	data := url.Values{}
+
+	data.Add("firstname", "safat")
+	data.Add("lastname", "mahmud")
+	data.Add("email", "safat@go.dev")
+
+	response, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
 }
