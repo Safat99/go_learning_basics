15bfe69c755a79cfc16b624561c2d30bf420d9e8
Safat99 <sarkersafat99@gmail.com>
2023-04-10 13:19:54 +0600

    delete func written

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -61,5 +61,30 @@ func updateOneMovie(movieId string) {
 	}
 
 	fmt.Println("total records are modified: ", result.ModifiedCount)
+}
 
+// delete 1 record
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+	deleteCount, err := collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie got deleted with delete count: ", deleteCount)
+}
+
+// delete all records from mongodb
+
+func deleteAllMovies() int64 {
+	deleteResult, err := collection.DeleteMany(context.Background(), bson.D{{}}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("total number of movies deleted: ", deleteResult.DeletedCount)
+	return deleteResult.DeletedCount
 }
